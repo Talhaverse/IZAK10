0f95dee003ee47ff828607438c0050726f76e9cb
MAC <ursanir@gmail.com>
2024-12-14 13:33:28 +0500

    fix

--- src/screens/customerdetail/CustomerInfo.js
+++ src/screens/customerdetail/CustomerInfo.js
@@ -1,7 +1,48 @@
-
+import { useEffect, useState } from 'react';
 import { View, Text,TouchableOpacity,ScrollView,StyleSheet } from 'react-native'
-import { MMKV } from 'react-native-mmkv'
+import { MMKV } from 'react-native-mmkv';
+import { fetchWrapper } from '../../components/helpers';
+
 const CustomerInfo = ({data,mode}) => {
+	const [showMore, setShowMore] = useState(false); // State to toggle view
+	const [company,setCompany] = useState([]);
+	const [product,setProduct] = useState([]);
+	useEffect(() => {
+       
+       loadCompanyList(data);
+       loadProductList()
+      }, []);
+
+	const loadCompanyList = async (data) => {
+        
+           const API_URL2 = process.env.API_URL
+            const url = `${API_URL2}/customer/company-list`;
+            
+           
+        
+            const test  = await fetchWrapper.get(url)
+            setCompany(test)
+        
+       
+           
+        
+    }
+
+    const loadProductList = async () => {
+        
+           const API_URL2 = process.env.API_URL
+            const url = `${API_URL2}/customer/product-list`;
+            
+           
+        
+            const test  = await fetchWrapper.get(url)
+            setProduct(test)
+            // console.log(test)
+        
+       
+           
+        
+    }
 	
 	 const storage = new MMKV({
       id: `izak-10`,
@@ -21,24 +62,14 @@ const CustomerInfo = ({data,mode}) => {
 		
 
 	  // }
-	  
+	const metaData = JSON.parse(data.customer.meta_data);
+
 	
 	return (
 		<View style={{marginTop:10}}>
 			<View style={styles.box}>
 				<View style={styles.value}>
 					<Text style={styles.textValue}>ID</Text>
-				</View>
-
-				<View style={styles.heading}>
-					<Text style={styles.textHeading}>{data.id}</Text>
-				</View>
-			</View>
-
-
-			<View style={styles.box}>
-				<View style={styles.value}>
-					<Text style={styles.textValue}>Customer T-ID</Text>
 				</View>
 
 				<View style={styles.heading}>
@@ -49,14 +80,34 @@ const CustomerInfo = ({data,mode}) => {
 
 			<View style={styles.box}>
 				<View style={styles.value}>
-					<Text style={styles.textValue}>Name</Text>
+					<Text style={styles.textValue}>Customer T-ID</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.tracking_id}</Text>
+				</View>
+			</View>
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Company Name</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{company.find(rs => rs.id === data?.customer?.company_id)?.name || data?.customer?.company_id}</Text>
+				</View>
+			</View>
+
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Customer Name</Text>
 				</View>
 
 				<View style={styles.heading}>
 					<Text style={styles.textHeading}>{data.customer.customer_name}</Text>
 				</View>
 			</View>
-
 
 			<View style={styles.box}>
 				<View style={styles.value}>
@@ -68,16 +119,126 @@ const CustomerInfo = ({data,mode}) => {
 				</View>
 			</View>
 
-
 			<View style={styles.box}>
 				<View style={styles.value}>
-					<Text style={styles.textValue}>Company Name</Text>
+					<Text style={styles.textValue}>Cycle Day</Text>
 				</View>
 
 				<View style={styles.heading}>
-					<Text style={styles.textHeading}>{data.customer.address}</Text>
+					<Text style={styles.textHeading}>{data.customer.cycle_day}</Text>
 				</View>
 			</View>
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Balance</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.balance}</Text>
+				</View>
+			</View>
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Due Date</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.due_date}</Text>
+				</View>
+			</View>
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Dpd Counter</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.dpd_counter}</Text>
+				</View>
+			</View>
+
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Product Type</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{product.find(rs => rs.id === data?.customer?.product_type_id)?.name || data?.customer?.product_type_id}</Text>
+				</View>
+			</View>
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Last payment Date</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.last_payment_date}</Text>
+				</View>
+			</View>
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Last payment Amount</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.last_payment_amount}</Text>
+				</View>
+			</View>
+
+
+			
+
+
+			
+
+			{showMore && (
+                <>
+                
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Mobile no</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.mobile}</Text>
+				</View>
+			</View>
+
+			<View style={styles.box}>
+				<View style={styles.value}>
+					<Text style={styles.textValue}>Email</Text>
+				</View>
+
+				<View style={styles.heading}>
+					<Text style={styles.textHeading}>{data.customer.email}</Text>
+				</View>
+			</View>
+
+			{Object.entries(metaData).map(([key, value], index) => (
+	            <View style={styles.box} key={index}>
+	                <View style={styles.value}>
+	                    <Text style={styles.textValue}>{key.replace(/_/g, ' ')}</Text>
+	                </View>
+	                <View style={styles.heading}>
+	                    <Text style={styles.textHeading}>{value}</Text>
+	                </View>
+	            </View>
+	        ))}
+
+			
+
+			
+			</>
+			)}
+
+			<TouchableOpacity onPress={() => setShowMore(!showMore)}>
+                <Text style={styles.toggleButton}>{showMore ? 'See Less' : 'See More'}</Text>
+            </TouchableOpacity>
 
 		</View>
 	)
@@ -103,6 +264,14 @@ export default CustomerInfo;
 
     textValue: {
         color:'#000',fontSize:15,fontWeight:'bold'
+    },
+        toggleButton: {
+        color: '#008B8B',
+        textAlign: 'center',
+        fontSize: 16,
+        marginTop: 10,
+        padding: 5,
+        fontWeight: 'bold',
     },
        
 
